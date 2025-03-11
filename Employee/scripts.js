34d577142536c5eb66d9323607722f9aab8fc30d
code <prowpriyakorn@gmail.com>
2025-03-11 17:39:16 +0700

    first commit

--- Employee/scripts.js
+++ Employee/scripts.js
@@ -16,5 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     profileButton.addEventListener("click", toggleProfileDropdown);
     document.addEventListener("click", closeDropdown);
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const profileButton = document.getElementById("profile-button");
+    const profileDropdown = document.getElementById("profileDropdown");
 
+    profileButton.addEventListener("click", function() {
+        profileDropdown.classList.toggle("active");
+    });
 
+    // ปิด dropdown เมื่อคลิกข้างนอก
+    document.addEventListener("click", function(event) {
+        if (!profileButton.contains(event.target) && !profileDropdown.contains(event.target)) {
+            profileDropdown.classList.remove("active");
+        }
+    });
+});
